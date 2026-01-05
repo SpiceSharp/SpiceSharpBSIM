8777589806fccc3a85f924c7a10631bacd519c72
Sven Boulanger <svenboulanger@hotmail.com>
2026-01-05 10:21:42 +0100

    Add BSIM2 extension.

---
+++ SpiceSharpBSIM.Parser/BSIM2Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SpiceSharp.Components;
+using SpiceSharpParser;
+using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.EntityGenerators.Components.Semiconductors;
+using SpiceSharpParser.ModelReaders.Netlist.Spice.Readers.EntityGenerators.Models;
+
+namespace SpiceSharpBSIM.Parser;
+
+/// <summary>
+/// A helper class that can extend Spice#.Parser with BSIM1 models.
+/// </summary>
+public static class BSIM2Extensions
+{
+    /// <summary>
+    /// Extends a <see cref="SpiceSharpReader"/> with the BSIM1 models.
+    /// </summary>
+    /// <param name="reader">The reader.</param>
+    /// <param name="level">The level.</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public static void UseBSIM2(this SpiceSharpReader reader, int level = 5)
+    {
+        // Register the model level
+        var nmosModelGenerator = reader.Settings.Mappings.Models.GetValue("NMOS", false);
+        var pmosModelGenerator = reader.Settings.Mappings.Models.GetValue("PMOS", false);
+        if (ReferenceEquals(nmosModelGenerator, pmosModelGenerator) &&
+            nmosModelGenerator is MosfetModelGenerator modelGenerator)
+            modelGenerator.AddGenericLevel<BSIM2Model, Components.Semiconductors.BSIM.BSIM2Behaviors.ModelParameters>(level);
+        else
+            throw new NotImplementedException();
+
+        // Register the mosfet component
+        if (reader.Settings.Mappings.Components.TryGetValue("M", false, out var generator) &&
+            generator is MosfetGenerator mosGenerator)
+            mosGenerator.AddMosfet<BSIM2Model, BSIM2>();
+        else
+            throw new NotImplementedException();
+    }
+}
---
+++ SpiceSharpBSIMTests/BSIM2Tests/BSIM2ParserTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SpiceSharpParser;
+using SpiceSharpBSIM.Parser;
+
+namespace SpiceSharpBSIMTests.BSIM2Tests;
+
+[TestFixture]
+public class BSIM2ParserTests
+{
+    [Test]
+    public void When_BSIM2Netlist_Expect_Reference()
+    {
+        string netlistContent = """
+            V1 g 0 0
+            V2 d 0 0
+            M1 d g 0 0 mod w=100u l=100u
+            .MODEL mod NMOS(LEVEL=5 vfb=-0.3 phi=0.8 k1=0.6 mu0=250 n0=1.3 tox=1e-7 mj=0.5 mjsw=0.33 pb=0.8 pbsw=1.0 xpart=1.0)
+            .DC V2 0 3.3 0.3 V1 0 3.3 0.3
+
+            .END
+            """;
+
+        var parser = new SpiceNetlistParser();
+        parser.Settings.Lexing.HasTitle = false;
+        var parseResult = parser.ParseNetlist(netlistContent);
+
+        // Convert to Spice#
+        var spiceSharpReader = new SpiceSharpReader();
+        spiceSharpReader.Settings.CaseSensitivity.IsModelTypeCaseSensitive = false;
+        spiceSharpReader.UseBSIM2();
+        var spiceSharpModel = spiceSharpReader.Read(parseResult.FinalModel);
+
+        Assert.That(spiceSharpModel.ValidationResult.HasError || spiceSharpModel.ValidationResult.HasWarning, Is.False);
+        Assert.That(spiceSharpModel.Simulations.Count, Is.EqualTo(1));
+
+        var simulation = spiceSharpModel.Simulations[0];
+        var codes = simulation.Run(spiceSharpModel.Circuit, -1);
+        foreach (int _ in simulation.InvokeEvents(codes)) ;
+    }
+}
